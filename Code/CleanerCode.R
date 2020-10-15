8a76d6b8b1da9092185d26f4fc84186624cc067c
Angelika Kurthen <kurthena@oregonstate.edu>
2020-10-15 10:41:03 -0700

    Cleaned Fort Huachuca data from 2013 and 2014

---
+++ Code/CleanerCode.R
@@ -0,0 +1,35 @@
+#########################
+# Raw Data Cleaning
+#########################
+
+# Fort Huachuca Data
+
+# 2013 Data from Garden and Huachuca Canyon
+Clean1(csv = "G1_April2013.csv")
+Clean1(csv = "G2_April2013.csv")
+Clean1(csv = "G3_April2013.csv")
+Clean1(csv = "G4_April2013.csv")
+Clean1(csv = "G5_April2013.csv")
+Clean1(csv = "G6_April2013.csv")
+Clean1(csv = "G7_April2013.csv")
+Clean1(csv = "H1_April2013.csv")
+Clean1(csv = "H2_April2013.csv")
+Clean1(csv = "H3_April2013.csv")
+Clean1(csv = "H4_April2013.csv")
+Clean1(csv = "H5_April2013.csv")
+Clean1(csv = "H6_April2013.csv")
+Clean1(csv = "H7_April2013.csv")
+
+# 2014 Data from Garden and Huachuca Canyon
+Clean1(csv = "G2_21.csv")
+Clean1(csv = "H1A_27_13Apr2014.csv")
+Clean1(csv = "H3_19_13Apr2014.csv")
+Clean1(csv = "H4_25_Apr2014.csv")
+Clean1(csv = "H5_68_14Apr2014.csv")
+Clean1(csv = "H6_37_Apr2014.csv")
+Clean1(csv = "H7_63_Apr2014.csv")
+
+#Note: Based on this, only ER sensors 
+#G2, H1, H3, H4, H5, H6 and H7 have 
+#continuous data from 2013 to 2014
+
