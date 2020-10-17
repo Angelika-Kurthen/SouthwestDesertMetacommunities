04db3335b8b409eb9dc34ea6ab1dc5236c512063
Angelika Kurthen <kurthena@oregonstate.edu>
2020-10-17 04:29:23 -0700

    Created code to specifically clean GFB_3 raw data; saved cleaned GFB_3 data to CleanData foldler

---
+++ GFB_3CleanerCode.R
@@ -0,0 +1,16 @@
+#######################################
+# Code to clean raw GFB_3 data
+#######################################
+
+library(readxl) # install necessary libraries 
+
+GFB_3 <- read_excel("Private-MetacommunityData/RawData/GFB composite (1).xlsx", 
+                    sheet = "12Nov13-3apr14 G3")
+
+# only select relevant columns and headers
+GFB_3Clean <- GFB_3[, c(2, 3, 4)] 
+
+# save file 
+write.csv(GFB_3Clean, 
+          file = 
+            paste0("Private-MetacommunityData/CleanData/","GFB_3Clean.csv"))
