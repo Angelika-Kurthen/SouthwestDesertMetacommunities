db0acc9b41e03d8541ec9ee0a0a7e800fb1ece5c
Angelika Kurthen <kurthena@oregonstate.edu>
2020-11-06 20:20:06 -0800

    Reformatted CleanerCode.R

--- Code/Data Prep/CleanerCode.R
+++ Code/Data Prep/CleanerCode.R
@@ -4,52 +4,54 @@
 # source functions to clean data
 source('Code/Data Prep/MetacommunityFunctions.R')
 
-#make into for loop 
-#pull paste0 outside clean1 function
+#make into for loop
+#pull paste
 
-# Fort Huachuca Data
+# make vector with data that can be prepped, cleaned, and created into clean data for
 # 2013 Data from Garden and Huachuca Canyon
-Clean1(csv = "G1_April2013.csv")
-Clean1(csv = "G2_April2013.csv")
-Clean1(csv = "G3_April2013.csv")
-Clean1(csv = "G4_April2013.csv")
-Clean1(csv = "G5_April2013.csv")
-Clean1(csv = "G6_April2013.csv")
-Clean1(csv = "G7_April2013.csv")
-Clean1(csv = "H1_April2013.csv")
-Clean1(csv = "H2_April2013.csv")
-Clean1(csv = "H3_April2013.csv")
-Clean1(csv = "H4_April2013.csv")
-Clean1(csv = "H5_April2013.csv")
-Clean1(csv = "H6_April2013.csv")
-Clean1(csv = "H7_April2013.csv")
-
 # 2014 Data from Garden and Huachuca Canyon
-Clean1(csv = "G2_21.csv")
-Clean1(csv = "H1A_27_13Apr2014.csv")
-Clean1(csv = "H3_19_13Apr2014.csv")
-Clean1(csv = "H4_25_Apr2014.csv")
-Clean1(csv = "H5_68_14Apr2014.csv")
-Clean1(csv = "H6_37_Apr2014.csv")
-Clean1(csv = "H7_63_Apr2014.csv")
+# 2014 Data from SAN1 and SAN2
+# 2014 Data from Ash Canyon
+# 2014 Data from Water Canyon
 
-#Note: Based on this, only ER sensors 
-#G2, H1, H3, H4, H5, H6 and H7 have 
-#continuous data from 2013 to 2014
+csvs <-
+  c(
+    "G1_April2013.csv",
+    "G2_April2013.csv",
+    "G3_April2013.csv",
+    "G4_April2013.csv",
+    "G5_April2013.csv",
+    "G6_April2013.csv",
+    "G7_April2013.csv",
+    "H1_April2013.csv",
+    "H2_April2013.csv",
+    "H3_April2013.csv",
+    "H4_April2013.csv",
+    "H5_April2013.csv",
+    "H6_April2013.csv",
+    "H7_April2013.csv",
+    "G2_21.csv",
+    "H1A_27_13Apr2014.csv",
+    "H3_19_13Apr2014.csv",
+    "H4_25_Apr2014.csv",
+    "H5_68_14Apr2014.csv",
+    "H6_37_Apr2014.csv",
+    "H7_63_Apr2014.csv",
+    "31_SAN_INT9Apr14.csv",
+    "48_SANSASL_9Apr14.csv",
+    "54_ASH_LAS_7Apr14.csv",
+    "35_ASH_MID_8Apr14.csv",
+    "51_ASH_UAL_8Apr14.csv",
+    "69_AshUpper_8Apr14.csv",
+    "12Wat_SFBP1Apr14.csv",
+    "16WAT1Apr2014.csv",
+    "17WATSFAC_1Apr2014.csv"
+  )
 
-#2014 Data from SAN1 and SAN2
+# add on full file name path so we can run through function
+csvpath <- paste0("Private-MetacommunityData/RawData/", csvs)
 
-Clean1(csv = "31_SAN_INT9Apr14.csv")  #SAN1 2014
-Clean1(csv = "48_SANSASL_9Apr14.csv") #SAN2 2014
-
-#2014 Data from Ash Canyon
-Clean1(csv = "54_ASH_LAS_7Apr14.csv") #ASH1 2014
-Clean1(csv = "35_ASH_MID_8Apr14.csv") #ASH2 2014
-Clean1(csv = "51_ASH_UAL_8Apr14.csv") #ASH3 2014
-Clean1(csv = "69_AshUpper_8Apr14.csv") #ASH4 2014
-
-
-#2014 WAT data
-Clean1(csv = "12Wat_SFBP1Apr14.csv") #WAT1 2014
-Clean1(csv = "16WAT1Apr2014.csv") #WAT2 2014
-Clean1(csv = "17WATSFAC_1Apr2014.csv") #WAT3 2014
+# loop through
+for (i in 1:length(csvs)) {
+  PrepAndCleanHoboCSV(csv = csvs[i], csvpath = csvpath[i])
+}
