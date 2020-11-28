e5be61656483a16a7bdb2c97bed0a5047ab000de
Angelika Kurthen <kurthena@oregonstate.edu>
2020-11-28 14:42:42 -0800

    added some code to to use OCH_ER_Check function for specific site season combos that have special if-statements in the function

--- Code/SpatialPrep/OCH_Data_ERSensor_Check.R
+++ Code/SpatialPrep/OCH_Data_ERSensor_Check.R
@@ -1,6 +1,11 @@
 #################################################################################
 # Code to check if dates of bug relevant OCH samples fits within ER sensor dates
 ##################################################################################
+
+library(lubridate)
+library(geosphere)
+library(stringr)
+
 # Check Ash Canyon Dates
 AC_Fall_2012 <- ER_OCH_Check(basin = "Ash Canyon", season = "Fall", year = "2012")  # Check dates
 AC_Spring_2013 <- ER_OCH_Check(basin = "Ash Canyon", season = "Spring", year = "2013")
@@ -29,3 +34,15 @@ GFB_Fall_2013 <- ER_OCH_Check(basin = "Great Falls Basin", season = "Fall", year
 SAN_Fall_2012 <- ER_OCH_Check(basin = "San Andres Canyon", season = "Fall", year = "2012") # check dates
 SAN_Spring_2013 <- ER_OCH_Check(basin = "San Andres Canyon", season = "Spring", year = "2013")
 SAN_Fall_2013 <- ER_OCH_Check(basin = "San Andres Canyon", season = "Spring", year = "2013")
+
+
+# Site/seasons that have missing ER sensors 
+
+# use ER_OCH_Check to write new if_statement and get the WC years that don't have WAT1 or WAT2 ER Sensor info
+WC_Fall_2013 <- ER_OCH_Check(basin = "Water Canyon without WAT1/WAT2", season = "Fall", year = "2013")
+WC_Spring_2013 <- ER_OCH_Check(basin = "Water Canyon without WAT1/WAT2", season = "Spring", year = "2013")
+
+# use ER_OCH_Check to write new if-statement for GC seasons that have limited number of sensors bc of flash flood
+GC_Fall_2013 <- ER_OCH_Check(basin = "Garden Canyon with G2 and G3 Only", season = "Fall", year = "2013")
+# HC Fall 2013
+HC_Fall_2013 <- ER_OCH_Check(basin = "Huachuca Canyon Fall 2013", season = "Fall", year = "2013")
