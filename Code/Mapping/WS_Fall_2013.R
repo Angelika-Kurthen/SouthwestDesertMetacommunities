9f3f6b5e211f4ab09c61beb55f4cf336560b480d
Angelika Kurthen <kurthena@oregonstate.edu>
2020-12-02 21:21:57 -0800

    changed map axes names

--- Code/Mapping/WS_Fall_2013.R
+++ Code/Mapping/WS_Fall_2013.R
@@ -14,7 +14,7 @@ WSF13 <- ggmap(base)
 X11()
 WSF13 + geom_point(data = WS_Fall_2013, aes(x = Longitude , y= Latitude, fill = FlowPermanence), shape = 21,cex = 4) +
   scale_fill_gradient2(low = "red", mid = "yellow", high = "blue", midpoint = 0.5, na.value = NA) +
-  labs(x="Latitude", y="Longitude", title= "White Sands, NM - Fall 2013") + # label the axes
+  labs(x="Longitude", y="Latitude", title= "White Sands, NM - Fall 2013") + # label the axes
   theme_bw() + theme(legend.position="bottom", legend.key = element_rect(colour = "white"), axis.text.x = element_text(angle=45, vjust=0.5))+
   geom_text(x= -106.5 , y=32.75, label="San Andres Canyon")+
   geom_text(x = -106.5, y = 32.64, label = "Ash Canyon" )
