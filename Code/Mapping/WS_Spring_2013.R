0616a375640e30de427b452790fac3aee734099c
Angelika Kurthen <kurthena@oregonstate.edu>
2020-12-02 19:40:43 -0800

    code to map White Sands, NM data from spring 2013

---
+++ Code/Mapping/WS_Spring_2013.R
@@ -0,0 +1,20 @@
+##################################################
+# Code to map for White Sands, NM data, spring 2013
+###################################################
+require(ggplot2)
+require(ggmap)
+
+range(WS_Spring_2013$Latitude)
+range(WS_Spring_2013$Longitude)
+
+base = get_map(location=c(-106.7,32.55,-106.4,32.8), zoom = 12, maptype="terrain-background")
+
+WSS13 <- ggmap(base)
+
+X11()
+WSS13 + geom_point(data = WS_Spring_2013, aes(x = Longitude , y= Latitude, fill = FlowPermanence), shape = 21,cex = 4) +
+  scale_fill_gradient2(low = "red", mid = "yellow", high = "blue", midpoint = 0.5, na.value = NA) +
+  labs(x="Latitude", y="Longitude", title= "White Sands, NM - Spring 2013") + # label the axes
+  theme_bw() + theme(legend.position="bottom", legend.key = element_rect(colour = "white"), axis.text.x = element_text(angle=45, vjust=0.5))+
+  geom_text(x= -106.5 , y=32.75, label="San Andres Canyon")+
+  geom_text(x = -106.5, y = 32.64, label = "Ash Canyon" )
