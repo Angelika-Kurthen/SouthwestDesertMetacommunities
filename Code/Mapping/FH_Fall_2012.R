4b1e96a45a07fc50693ac8f4fce26656e236e0d0
Angelika Kurthen <kurthena@oregonstate.edu>
2020-12-02 19:39:01 -0800

    code to map Fort Huachuca, AZ sites for Fall 2012

---
+++ Code/Mapping/FH_Fall_2012.R
@@ -0,0 +1,19 @@
+####################################################
+# Code to map for Fort Huachuca, AZ data, Fall 2012
+####################################################
+require(ggplot2)
+require(ggmap)
+
+range(FH_Fall_2012$Latitude)
+range(FH_Fall_2012$Longitude)
+FHbase = get_map(location=c(-110.45,31.42,-110.3,31.55), zoom = 12, maptype="terrain-background")
+
+FHF12 <- ggmap(FHbase)
+
+X11()
+FHF12+ geom_point(data = FH_Fall_2012, aes(x = Longitude , y= Latitude, fill = FlowPermanence), shape = 21,cex = 4) +
+  scale_fill_gradient2(low = "red", mid = "yellow", high = "blue", midpoint = 0.5, na.value = NA) +
+  labs(x="Latitude", y="Longitude", title= "Fort Huachuca, AZ - Fall 2012") + # label the axes
+  theme_bw() + theme(legend.position="bottom", legend.key = element_rect(colour = "white"), axis.text.x = element_text(angle=45, vjust=0.5))+
+  geom_text(x= -106.5 , y=32.75, label="San Andres Canyon")+
+  geom_text(x = -106.5, y = 32.64, label = "Ash Canyon" )
